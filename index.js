a28691defb88351980f5edde0f4e9615a9ec9307
developer-munir <devolopermunir@gmail.com>
2022-11-08 00:22:25 +0600

    services api create and connect mongodb

--- index.js
+++ index.js
@@ -9,11 +9,36 @@ app.use(express());
 require("dotenv").config();
 
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.asxi1ae.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    const servicesCollection = client.db('weddingDb').collection('services');
+    try {
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+        
+    }
+}
+run().catch(error=>console.error(error))
+
 
 app.get('/', (req, res) => {
     res.send('Assignment 11 server running..');
 })
 app.listen(port, () => console.log(`Server running on port: ${port}`));
+
 
 
 
