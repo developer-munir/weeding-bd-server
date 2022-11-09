709c44fba0cecec51eb78e484d92aa359036eb48
developer-munir <devolopermunir@gmail.com>
2022-11-09 22:34:32 +0600

    implement jwt token

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.asxi1ae.mongodb.net/?retryWrites=true&w=majority`;
@@ -16,10 +17,36 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+
+// verify jwt
+function verifyJWT(req, res, next) {
+  // next(); 
+  // console.log(req.headers.authorization);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error,decoded) => {
+    if (error) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 async function run() {
   const servicesCollection = client.db("weddingDb").collection("services");
   const postCollection = client.db("weddingDb").collection("allpost");
   try {
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn:'10h'
+      });
+      res.send({token})
+    })
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = servicesCollection.find(query);
@@ -31,6 +58,11 @@ async function run() {
       const cursor = servicesCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+    app.post("/addservice", async (req, res) => {
+      const post = req.body;
+      const result = await servicesCollection.insertOne(post);
+      res.send(result);
     });
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
@@ -49,9 +81,13 @@ async function run() {
       const allpost = await cursor.toArray();
       res.send(allpost);
     });
-    app.get("/reviews", async (req, res) => {
+    app.get("/reviews", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      if (decoded.email !== req.query.email) {
+        res.status(403).send({ message: 'unauthorized access' });
+      }
       let query = {};
-      //   console.log(req.query)
+        // console.log(req.headers)
       if (req.query.email) {
         query = {
           email: req.query.email,
