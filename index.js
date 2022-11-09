ccacd827a85621e1666062c4aa3652340e16b59d
developer-munir <devolopermunir@gmail.com>
2022-11-09 15:07:15 +0600

    create update api

--- index.js
+++ index.js
@@ -41,34 +41,52 @@ async function run() {
     app.post("/allposts", async (req, res) => {
       const post = req.body;
       const result = await postCollection.insertOne(post);
-      res.send(result); 
+      res.send(result);
     });
-      app.get('/allposts', async (req, res) => {
-          const query = {};
-          const cursor = postCollection.find(query);
-          const allpost = await cursor.toArray();
-          res.send(allpost);
-    }) 
-      app.get('/reviews', async (req, res) => {
-          let query = {};
-        //   console.log(req.query)
-          if (req.query.email) {
-              query = {
-                  email:req.query.email
-              }
-          }
-          const cursor = postCollection.find(query);
-          const allpost = await cursor.toArray();
-          res.send(allpost);
-      }) 
-    app.delete('/reviews/:id', async(req, res) => {
+    app.get("/allposts", async (req, res) => {
+      const query = {};
+      const cursor = postCollection.find(query);
+      const allpost = await cursor.toArray();
+      res.send(allpost);
+    });
+    app.get("/reviews", async (req, res) => {
+      let query = {};
+      //   console.log(req.query)
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = postCollection.find(query);
+      const allpost = await cursor.toArray();
+      res.send(allpost);
+    });
+    app.delete("/reviews/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await postCollection.deleteOne(query);
       res.send(result);
-    })
-  }
-  finally {
+    });
+    app.put("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const user = req.body;
+      console.log(user)
+      const option = { upsert: true };
+      const updatedUser = {
+        $set: {
+          review: user.review,
+        },
+      };
+      const result = await postCollection.updateOne(
+        filter,
+        updatedUser,
+        option
+      );
+      // console.log(user);
+      res.send(result);
+    });
+  } finally {
   }
 }
 run().catch((error) => console.error(error));
